ce3e4e968cba209def65d84868ff4abdc2194146
bgb0923 <bgb0923@gmail.com>
2017-02-21 17:44:59 -0500

    Finished Lab

--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -3,13 +3,35 @@
 //  Hospital
 
 
+struct AdvisoryBoardMember {
+    let name: String
+    var vacationDays: Int = 30
+    
+    func wages() -> Double {
+        return 100_000.00
+    }
+}
 
+extension AdvisoryBoardMember: Payable, TimeOff {
+    
+}
 
+struct Principal {
+    let name: String
+    var vacationDays: Int = 20
+}
 
+extension Principal: Payable, TimeOff, Reprimand {
+    func wages() -> Double {
+        return 80_000
+    }
+}
 
+struct Teacher {
+    let name: String
+    var vacationDays: Int = 15
+}
 
-
-
-
-
-
+extension Teacher: Payable, TimeOff, Teach {
+    
+}
